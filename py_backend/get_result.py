bc4ba5537d1ca8c67e8295499f062d389451f661
scott <st.scott0612@gmail.com>
2025-05-24 18:54:40 +0100

    replace deprecated model

--- py_backend/get_result.py
+++ py_backend/get_result.py
@@ -18,7 +18,7 @@ def generate(img_path):
         # Please ensure that the file is available in local system working direrctory or change the file path.
         client.files.upload(file=img_path),
     ]
-    model = "gemini-2.0-flash-thinking-exp-01-21"
+    model = "gemini-2.5-flash-preview-05-20"
     contents = [
         types.Content(
             role="user",
